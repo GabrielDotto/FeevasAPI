cb596ba0d761c5da53840145026c5ef8eb546812
GabrielDotto <gabrielfalkembach@gmail.com>
2017-10-19 00:11:13 -0200

    Agora vai

--- Trab1Prog4/Controllers/HomeController.cs
+++ Trab1Prog4/Controllers/HomeController.cs
@@ -8,23 +8,18 @@ namespace Trab1Prog4.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Register()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
---
+++ trabalho1/Models/Log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace trabalho1.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Acao { get; set; }
+    }
+
+}
